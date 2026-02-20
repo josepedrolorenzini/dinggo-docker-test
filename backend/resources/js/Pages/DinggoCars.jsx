145961b0b5592f4cf0842df1dc28e1025c99ee16
josepedrolorenzini <webmaster@marbleceramiccorp.com.au>
2026-02-21 10:09:03 +1100

    adding style to my dinggocars component

--- backend/resources/js/Pages/DinggoCars.jsx
+++ backend/resources/js/Pages/DinggoCars.jsx
@@ -1,5 +1,23 @@
 import { Head } from '@inertiajs/react'
-import React from 'react'
+import React from 'react';
+
+const containerStylo = {
+    display: 'grid',
+    // This creates 3 columns of equal width
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gap: '15px',
+    padding: '20px',
+    color: '#333',
+};
+const boxStylo = {
+    border: '1px solid #ccc',
+    padding: '15px',
+    borderRadius: '8px',
+    backgroundColor: '#f9f9f9',
+    boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+    display: 'flex',
+    flexDirection: 'column'
+};
 
 function DinggoCars({ cars }) {
     return (
@@ -7,11 +25,21 @@ function DinggoCars({ cars }) {
             <Head title="Dinggo Cars" />
             <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
                 <h1>Dinggo Cars</h1>
-                <ul>
-                    {cars.map(car => (
-                        <li key={car.vin}>{car.make} {car.model}</li>
-                    ))}
-                </ul>
+                {/* grid container */}
+                <div style={containerStylo}>
+                    <ul
+                        className='list-disc list-inside'>
+                        {cars && cars.map(car => (
+                            <div
+                                style={boxStylo}
+                                key={car.vin}
+                            >
+                                <li key={car.vin}>{car.make} {car.model}</li>
+                            </div>
+
+                        ))}
+                    </ul>
+                </div>
             </div>
         </>
     )
--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -16,7 +16,7 @@
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white">
+    <body class="font-sans antialiased dark:bg-black dark:text-white ">
         @inertia
     </body>
 </html>
