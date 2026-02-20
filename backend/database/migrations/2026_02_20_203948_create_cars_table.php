b9ae14abd87efe2475a57a8fde210b122d295eaa
josepedrolorenzini <webmaster@marbleceramiccorp.com.au>
2026-02-21 09:34:29 +1100

    cars model nullable() improvement

--- backend/database/migrations/2026_02_20_203948_create_cars_table.php
+++ backend/database/migrations/2026_02_20_203948_create_cars_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,13 +12,13 @@ return new class extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
-            $table->string('colour' ,60);
-            $table->string('license_plate',50);
-            $table->string('license_state',50);
-            $table->string('make',100);
-            $table->string('model',100);
-            $table->string('vin',20)->unique();
-            $table->integer('year');
+            $table->string('colour', 60)->nullable();
+            $table->string('license_plate', 50)->nullable();
+            $table->string('license_state', 50)->nullable();
+            $table->string('make', 100)->nullable();
+            $table->string('model', 100)->nullable();
+            $table->string('vin', 20)->unique();
+            $table->integer('year')->nullable();
             $table->timestamps();
         });
     }
