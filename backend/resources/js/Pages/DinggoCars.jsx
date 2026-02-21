edb2d7a71ef14ebe30e5ca58ade1d80d0472daf8
josepedrolorenzini <webmaster@marbleceramiccorp.com.au>
2026-02-22 07:57:31 +1100

    updating react components

--- backend/resources/js/Pages/DinggoCars.jsx
+++ backend/resources/js/Pages/DinggoCars.jsx
@@ -61,6 +61,12 @@ function DinggoCars({ auth, laravelVersion, phpVersion, cars }) {
                                         Home
                                     </Link>
                                     <Link
+                                        href={route("cars.show")}
+                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Cars
+                                    </Link>
+                                    <Link
                                         href={route('login')}
                                         className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
