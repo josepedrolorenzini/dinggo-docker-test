8d467e46da362bd146bf00c699721ac620aaf994
josepedrolorenzini <webmaster@marbleceramiccorp.com.au>
2026-02-21 11:26:27 +1100

    adding routes() components

--- backend/resources/js/Pages/DinggoCars.jsx
+++ backend/resources/js/Pages/DinggoCars.jsx
@@ -55,6 +55,12 @@ function DinggoCars({ auth, laravelVersion, phpVersion, cars }) {
                             ) : (
                                 <>
                                     <Link
+                                        href={route("home")}
+                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Home
+                                    </Link>
+                                    <Link
                                         href={route('login')}
                                         className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
